a274c7abccc97d95780541d59c2a69b5d26ea48d
Yuri Paiva <yuriqpaiva@gmail.com>
2023-05-03 14:40:38 -0300

    Add radix ui radio group components

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { CloseButton, Content, Overlay } from './styles';
-import { X } from '@phosphor-icons/react';
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from '@phosphor-icons/react';
 
 export function NewTransactionModal() {
   return (
@@ -18,9 +24,19 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
 
+          <TransactionType>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">Cadastrar</button>
         </form>
-
       </Content>
     </Dialog.Portal>
   );
