1356d75ec041811ccf6ac6f3c053dd7cb42469e9
Yuri Paiva <yuriqpaiva@gmail.com>
2023-05-03 19:41:50 -0300

    Add formatters

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,9 +6,29 @@ import {
 import { SummaryCard, SummaryContainer } from './styles';
 import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormatter } from '../../utils/formatter';
 
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
 
   return (
     <SummaryContainer>
@@ -17,17 +37,15 @@ export function Summary() {
           <span>Entradas</span>
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
-
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
-
       <SummaryCard>
         <header>
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -36,7 +54,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#ffffff" />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
